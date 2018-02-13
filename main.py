7e6a10a8ea6a238520dee2fdb384a9c68cc0ac66
CrabbeHolley <wangpangbo@sina.cn>
2018-02-13 09:54:17 +0800

    嫌疑人选择（完成一半）

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys;
+import pymysql;
 from XYR import Ui_MainWindow;
 from jubao import Ui_Dialog as Ui_Jubao;
 from suspect import Ui_Dialog as Ui_Suspect;
@@ -8,9 +9,12 @@ class mywindow(QtWidgets.QMainWindow,Ui_MainWindow):
     def __init__(self):
         super(mywindow,self).__init__()
         self.setupUi(self)
+        for row in names:
+            self.comboBox.addItem(row[1])
     def action_in(self):
         self.addSuspectAction = jubaoWidget()
         self.addSuspectAction.show()
+        print(selected)
     def suspect_in(self):
         self.addSuspect = suspectWidget()
         self.addSuspect.show()
@@ -29,7 +33,15 @@ class suspectWidget(QtWidgets.QDialog,Ui_Suspect):
     def back(self):
         self.close()
 
+
+db = pymysql.connect("localhost","root","crab1996","suspect",0,None,"utf8")
+cursor = db.cursor()
+cursor.execute("select * from suspect_position")
+names = cursor.fetchall()
+
+selected = "0001"
 app = QtWidgets.QApplication(sys.argv)
 window = mywindow()
 window.show()
+db.close()
 sys.exit(app.exec_())
